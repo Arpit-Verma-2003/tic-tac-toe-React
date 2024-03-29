28d5cc1093f96607309930ff36badc142162742a
Arpit-Verma-2003 <arpitverma200316@gmail.com>
2024-03-29 23:00:28 +0530

    board changes

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,9 +7,29 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
-  return <ol id="game-board">{initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
-    <ol>
-        {row.map((symbol,colIndex)=><li key={colIndex}><button>{symbol}</button></li>)}
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+  function handlePlayerMove(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const newGameBoard = [
+        ...prevGameBoard.map(innerArray => [...innerArray]),
+      ];
+      newGameBoard[rowIndex][colIndex] = "X";
+      return newGameBoard;
+    });
+  }
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((symbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={()=>handlePlayerMove(rowIndex,colIndex)}>{symbol}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
     </ol>
-  </li>)}</ol>;
+  );
 }
